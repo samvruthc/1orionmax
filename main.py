ee6c3b2b0b7586ef7ab68c84d8bf506928809bdc
samvruthc <samvruthc@gmail.com>
2026-05-21 07:45:20 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
 from fastapi.responses import FileResponse
